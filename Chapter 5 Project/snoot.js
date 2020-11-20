1dabf78d657ebdf9b9d28dd2d499ab6a48cc40c9
Austin <43624199+Atmosphere9999@users.noreply.github.com>
2020-11-20 12:28:47 -0800

    Update snoot.js

--- Chapter 5 Project/snoot.js
+++ Chapter 5 Project/snoot.js
@@ -1,88 +1,88 @@
-// interpret document contents in JS strict mode
-"use strict"; 
+"use strict"; // interpret document contents in JS Strict mode
 
-/* Global Variables */
+/* Global Variables */ 
 var twentyNine = document.createDocumentFragment(); 
-var thirty = document.createDocumentFragment();
-var thirtyOne = document.createDocumentFragment();
+var thirty = document.createDocumentFragment(); 
+var thirtyOne = document.createDocumentFragment(); 
 
-/* set up node building blocks for selection list of days */
-
-function setupDays () {
-    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+/* set up node building blocks for selection list of days */ 
+function setupDays() {
+    var dates = document.getElementById("delivDy").
+    getElementsByTagName("option"); 
     twentyNine.appendChild(dates[28].cloneNode(true));
-    // Add 29th
-    thirty.appendChild(dates[28].cloneNode(true)); 
+    // add 29th
+    thirty.appendChild(dates[28].cloneNode(true));
     thirty.appendChild(dates[29].cloneNode(true)); 
-    // Add 29th & 30th
-    thirtyOne.appendChild(dates[28].cloneNode(true));
+    // add 29th & 30th
+    thirtyOne.appendChild(dates[28].cloneNode(true)); 
     thirtyOne.appendChild(dates[29].cloneNode(true));
-    thirtyOne.appendChild(dates[30].cloneNode(true)); 
-    // Add 29th, 30th & 31st
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+    // add 29th, 20th and 31st
 }
-function updateDays () {
-    var deliveryDay = document.getElementById("delivDy"); 
+
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
     var dates = deliveryDay.getElementsByTagName("option");
-    var deliveryMonth = document.getElementById("delivMo"); 
-    var deliveryYear = document.getElementById("delivYr");
-    var selectedMonth = deliveryMonth.options 
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("DelivYr");
+    var selectedMonth = deliveryMonth.options
     [deliveryMonth.selectedIndex].value; 
     while (dates[28]) {
         // remove child with index of 28 until this index is empty
-        deliveryDay.removeChild(dates[28]); 
+        deliveryDay.removeChild(dates[28]);
     }
         if (deliveryYear.selectedIndex === -1) {
-            // if no year is selected, choose the default year so length of Feb can be determined
+            // if no year is selected, choose the default year so length of Feb can be determined. 
             deliveryYear.selectedIndex = 0;
         }
-        if (selectedMonth === "2" &&
-        deliveryYear.options[deliveryYear.selectedIndex].value === "2018") {
-            // if leap year, Feb has 29 days
-            deliveryDay.appendChild(twentyNine.cloneNode(true)); 
-         } else if (selectedMonth === "4" || selectedMonth === "6" ||
-            selectedMonth === "9" || selectedMonth === "11") {
-                // these months have 30 days
-                deliveryDay.appendChild(thirty.cloneNode(true)); 
-        } else if (selectedMonth === "1" || selectedMonth === "3" ||
-        selectedMonth === "5" || selectedMonth === "7" ||
-        selectedMonth === "8" || selectedMonth === "10" ||
-        selectedMonth === "12") {
-            // these months have 31 days
-            deliveryDay.appendChild(thirtyOne.cloneNode(true)); 
-         }
+            if (selectedMonth === "2" &&
+            deliveryYear.options[deliveryYear.selectedIndex].value === "2018") {
+                deliveryDay.appendChild(twentyNine.cloneNode(true));
+            } else if (selectedMonth === "4" || selectedMonth === "6" ||
+                selectedMonth === "9" || selectedMonth === "11") {
+                    deliveryDay.appendChild(thirty.cloneNode(true));
+                } else if (selectedMonth === "1" || selectedMonth === "3" ||
+                    selectedMonth === "5" || selectedMonth === "7" ||
+                    selectedMonth === "8" || selectedMonth === "10" ||
+                    selectedMonth === "12") {
+                        deliveryDay.appendChild(thirtyOne.cloneNode(true)); 
+}
+}
+
 
 /* remove default values and formatting from state and delivery date selection lists */ 
-
 function removeSelectDefaults() {
-    var emptyBoxes = document.getElementsByTagName("select");
+    var emptyBoxes = document.getElementsByTagName("select"); 
     for (var i = 0; i < emptyBoxes.length; i++) {
-        emptyBoxes[i].selectedIndex = -1;
+        emptyBoxes[i].selectedIndex = -1; 
     }
 }
 
-/* create event listeners */ 
-function createEventListeners () {
+/* create event listeners */
+function createEventListeners() {
     var deliveryMonth = document.getElementById("delivMo"); 
     if (deliveryMonth.addEventListener) {
         deliveryMonth.addEventListener("change", updateDays, false); 
-    } else if (deliveryMonth.attachEvent) {
+    } else if (deliveryMonth.attachEvent) 
         deliveryMonth.attachEvent("onchange", updateDays); 
         var deliveryYear = document.getElementById("delivYr"); 
         if (deliveryYear.addEventListener) {
-            deliveryYear.addEventListener("change", updateDays, false); 
+            deliveryYear.addEventListener("change", updateDays, false);
         } else if (deliveryYear.attachEvent) {
             deliveryYear.attachEvent("onchange", updateDays); 
-        }
+}
+}
 
-/* Run initial form configuaration functions */
-function setUpPage () {
+/* run initial form config functions */ 
+function setUpPage() {
     removeSelectDefaults();
     setupDays(); 
     createEventListeners(); 
-   }
+}
+
 /* run setup function when page finishes loading */
 if (window.addEventListener) {
-    window.addEventListener("load", setUpPage, false);
+    window.addEventListener("load", setUpPage, false); 
 } else if (window.attachEvent) {
-    window.attachEvent("onload", setUpPage); 
+    window.attachEvent("onload", setUpPage);
 }
